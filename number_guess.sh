8e86c8d00ccd71ac8770ed4fe28a17d1ff559ee6
BThanh T <tranbaothanh2208@gmail.com>
2024-11-01 16:25:15 +0000

    feat: check username and welcome existing user

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,22 @@
 
 PSQL='psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c'
 
+echo "Enter your username:"
+read USERNAME
 
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'");
+
+
+if [[ -z $USER_ID ]]
+then
+  echo Welcome, $USERNAME! It looks like this is your first time here.
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+
+else
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE user_id = $USER_ID")
+
+  echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+fi
 
 
